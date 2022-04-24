dab40a90e3ab64ccb2aa4509ec0fbd61335c5732
p1zza <edu.hen.fm@gmail.com>
2022-04-24 09:29:56 -0400

    now it saves the imgs in a pdf

--- manga-cli-br.sh
+++ manga-cli-br.sh
@@ -4,34 +4,29 @@
 # INFO #
 ########
 
-version="0.1a"
-dependencies=("curl" "sed" "awk" "tr" "rm")
+version="0.2a"
+img_dir="$HOME/Documentos/manga-cli-using-mangalivre/imgs"
+# dependencies=("curl" "sed" "awk" "tr" "rm")
 
 
 ####################
 # FORMAT FUNCTIONS #
 ####################
 
-# Format Input
 format_search() {
     formatted_search="$(printf "%s" "${manga_to_search}" | tr " -" "+" | sed 's/^.//')"
-    # printf gets the text to work on
-    # tr replaces all spaces and hiphens with +
-    # sed is replacing the first character of the string with nothing (removing it)
 }
 
 ####################
 # SCRAPE FUNCTIONS #
 ####################
 
-# Get Titles
 get_titles_and_links() {
     not_found="$(curl --silent "https://muitomanga.com/buscar?q=${formatted_search}" | grep -c "Nenhum resultado encontrado")"
 
     if [ "${not_found}" -ge 1 ]; then
         echo "Mangá não foi encontrado"
     else
-        # ORIGINAL curl --silent "https://muitomanga.com/buscar?q=${formatted_search}" | grep "</a></h3>" | awk -F'>|</a>' '{print $3}' > titles-and-links.txt
         curl --silent "https://muitomanga.com/buscar?q=${formatted_search}" | grep "</a></h3>" | awk -F'<a |</a>' '{print $2}' > titles-and-links.txt
         awk -F'/|"' '{print $4}' titles-and-links.txt > links.txt &
         awk -F'>' '{print $2}' titles-and-links.txt > titles.txt
@@ -40,7 +35,6 @@ get_titles_and_links() {
     titles_amount=$(wc -l titles.txt | awk '{print $1}')
 }
 
-# Get Chapters
 get_chapters() {
     curl --silent "https://muitomanga.com/manga/${manga_link}" | grep "class=\"single-chapter\" data-id-cap=\"" | awk -F'"' '{print $4}' > chapters.txt
     echo -n "[$(tail -n 1 chapters.txt)~$(head -n 1 chapters.txt)]"
@@ -60,19 +54,59 @@ get_imgs() {
         i=$((i+2))
     done
     
-    img_urls=($(sed 's/\\//g' imgs2.txt))
+    sed 's/\\//g' imgs2.txt | grep '\S' > imgs.txt
+    mapfile -t img_urls < <(cat imgs.txt)
 
-    mkdir imgs
+    mkdir "${img_dir}"
     i=0
     for image in "${img_urls[@]}"; do
         i=$((i+1))
-        curl --silent --create-dirs --header 'Referer: https://muitomanga.com/' --output "imgs/${i}.jpg" "${image}" &
+        curl --silent --create-dirs --header 'Referer: https://muitomanga.com/' --output "${img_dir}/${i}.jpg" "${image}" &
     done
+
+    i=0
+    for image in "${img_urls[@]}"; do
+        i=$((i+1))
+        echo -n "${img_dir}/${i}.jpg " >> testing.txt
+    done
+
+    wait
+}
+
+get_pdf() {
+    echo "Convertendo imagens em PDF..."
+    
+    img2pdf $(cat testing.txt) --output "result.pdf"
+    rm -r "${img_dir}"
+    clear
 }
 
 ###################
 # PRINT FUNCTIONS #
 ###################
+
+print_help() {
+    while IFS= read -r line; do
+        printf "%s\n" "${line}"
+    done <<-EOF         
+    
+                                                   _ _        _          
+                                                  | (_)      | |         
+     _ __ ___   __ _ _ __   __ _  __ _         ___| |_       | |__  _ __ 
+    | '_ ' _ \ / _' | '_ \ / _' |/ _' | ____  / __| | | ____ | '_ \| '__|
+    | | | | | | (_| | | | | (_| | (_| | |__| | (__| | | |__| | |_) | |   
+    |_| |_| |_|\__._|_| |_|\__. |\__._|       \___|_|_|      |_.__/|_|   
+                            __/ |                                       
+                           |___/                     [Versão: ${version}]
+
+    Script em Bash para ler mangá usando seu terminal!
+
+    Como usar: ${0} [Opção]
+    Opções:
+    -h ou --help: mostra esta tela
+    -d ou --debug: não exclui os arquivos temporários gerados pelo script
+EOF
+}
 
 print_mangas() {
     i=1
@@ -127,6 +161,22 @@ choose_chapter() {
 # START UP #
 ############
 
+#####################
+# VERIFYING OPTIONS # 
+#####################
+
+case "${1}" in
+    -h|--help)
+        print_help
+    ;;
+    -d|--debug)
+        echo "WIP"
+    ;;
+    *)
+    ;;
+esac
+
+
 search_input
 format_search
 get_titles_and_links
@@ -134,5 +184,6 @@ print_mangas
 choose_manga
 choose_chapter
 get_imgs
+get_pdf
 
-rm imgs
+# rm imgs
